7ba2e26a5d85a57572046f73c58f4a1fc37b33ba
WulfMarius <WulfMarius@users.noreply.github.com>
2017-10-11 20:28:01 +0200

    upgraded to ModComponent 1.2.0 and replaced initialization with Auto Mapper

---
+++ Unity/Assets/Editor/AddSpawnLocation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+using UnityEngine;
+using UnityEditor;
+
+using ModComponentAPI;
+
+public class AddLootTableEntry : MonoBehaviour
+{
+    private const string TARGET_DIRECTORY = "AssetBundles";
+
+    [MenuItem("Component/Add SpawnLocation")]
+    public static void Execute()
+    {
+        GameObject gameObject = Selection.activeObject as GameObject;
+        if (gameObject == null)
+        {
+            return;
+        }
+
+        TextEditor editor = new TextEditor();
+        editor.Paste();
+        string json = editor.text;
+
+        ModSpawnLocationComponent spawnLocation = gameObject.AddComponent<ModSpawnLocationComponent>();
+        JsonUtility.FromJsonOverwrite(json, spawnLocation);
+    }
+}
